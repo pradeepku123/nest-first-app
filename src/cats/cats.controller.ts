615babd95ba6b146151e4a5db8cc1fd266fd9f67
pradeepku123 <pradeepkumarstudent2011@gmail.com>
2023-12-27 12:06:43 +0530

    add cats

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+  @Get()
+  getCats() {
+    return this.catsService.getCats();
+  }
+}
